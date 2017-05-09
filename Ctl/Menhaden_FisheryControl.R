455651bae9c8f55a7ae80f94e1bc64a06c77c8b6
Margaret Siple <mcsiple@gmail.com>
2017-05-09 14:10:21 -0700

    fixed comments

--- Ctl/Menhaden_FisheryControl.R
+++ Ctl/Menhaden_FisheryControl.R
@@ -1,8 +1,8 @@
-#Menhaden fishery control file (NEED TO FIX BECAUSE I DON'T ACTUALLY HAVE ALL THE RIGHT VALUES IN HERE!)
+#Menhaden fishery control file (NEED TO FIX BECAUSE I DON'T ACTUALLY HAVE ALL THE RIGHT VALUES HERE!)
+# Menhaden parameters come from 
 tot <- sum(c(6.221,4.170,2.795,1.873,1.256,0.842,0.564))
 init.prop <- c(6.221,4.170,2.795,1.873,1.256,0.842,0.564)/tot 
 init.B <- init.prop*200000 # Biomass at age -- 110.54 is the B0 when R0 = 1000
 init.test <- init.B / lh.test$w.at.age # These are all the numbers at age (spawning and non-spawning biomass)
 F0.test <- 0.3 # This is arbitrary
-cr.test <- list(Blim = 100, Btarget = 200, Fmax = 0.6) 
-# Testing whether I can use this w Git
+# cr.test <- list(Blim = 100, Btarget = 200, Fmax = 0.6) 
