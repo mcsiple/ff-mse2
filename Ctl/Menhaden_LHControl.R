839912c9c07b0e23e95e8fa6cc2f0c01e04fea19
Margaret Siple <mcsiple@gmail.com>
2017-05-09 14:01:29 -0700

    added references to menhaden fishery ctl

--- Ctl/Menhaden_LHControl.R
+++ Ctl/Menhaden_LHControl.R
@@ -1,14 +1,14 @@
 # Menhaden_LHControl
 # Life history params for the menhaden model, to be used with menhaden-like recruitment patterns
-
+# These are based on the Atlantic menhaden stock assessment
 ages.test <- 0:6
 nages.test <- length(ages.test)
 selectivity.test <- cbind(age=ages.test,selectivity = c(0.004,0.143,0.994,0.84,0.191,0.024,0))  # "domed" commercial selectivity from reduction fishery. Used Butterworth 2012 as an example
-lh.test <- list(M = 0.5,   #from menhaden assessment (http://www.asmfc.org/uploads/file/55089931S40_AtlMenhadenSAR_CombinedFINAL_1.15.2015-reduced.pdf) - mortality is averaged over all ages, from all years (Boudreau & Dickie)
+lh.test <- list(M = 0.5,   #from menhaden assessment (http://www.asmfc.org/uploads/file/55089931S40_AtlMenhadenSAR_CombinedFINAL_1.15.2015-reduced.pdf) - mortality is averaged over all ages, from all years (Boudreau & Dickie) - table 3.6.1
                 selectivity = selectivity.test,
                 ages = ages.test,
-                l.at.age = seq(4,28,length.out=nages.test),
+                l.at.age = seq(4,28,length.out=nages.test), #http://www.asmfc.org/uploads/file/55089931S40_AtlMenhadenSAR_CombinedFINAL_1.15.2015-reduced.pdf (Table 3.3.2)
                 w.at.age = c(0.0569, 0.1281,0.2317,0.3285,0.3711,0.5371,0.4481)*0.001, #weights at age are in kg  I think, multiply by 0.001 to get mt
-                maturity = c(0,0.13,0.53,0.83,0.98,1,1), 
+                maturity = c(0,0.13,0.53,0.83,0.98,1,1), # Table 3.4.1
                 R0=1e9) # R0 is flexible, and can be changed. I made it up.
 
