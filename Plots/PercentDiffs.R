1f7c1c6eff5b0385dc54b907ec56a82c22cf97db
Margaret Siple <mcsiple@gmail.com>
2017-08-03 10:30:02 -0700

    edits for +/-

--- Plots/PercentDiffs.R
+++ Plots/PercentDiffs.R
@@ -1,9 +1,10 @@
+library(car)
 library(plyr)
 library(dplyr)
 library(reshape2)
 library(ggplot2)
 library(RColorBrewer)
-dat <- read.csv("/Users/mcsiple/Dropbox/Chapter4-HarvestControlRules/Results/FF_MSE_summary.csv")
+dat <- read.csv("~/Dropbox/Chapter4-HarvestControlRules/Results/FF_MSE_summary.csv")
 head(dat)
 dat <- subset(dat, h==0.6,obs.error.type != "noerror")
 remove <- c("scenario","M.type","h","recruit.rho","recruit.sd")
@@ -24,7 +25,9 @@ dat3 <- mutate(dat3,HCR = recode(HCR, 'cfp' = 'Stability-favoring',
                             "constF" = "Constant F - low",
                             'constF_HI' = "Constant F - high"))
 
-write.csv(dat3,"PercentDiffs.csv")
+#write.csv(dat3,"PercentDiffs.csv")
+setwd("~/Dropbox/Chapter4-HarvestControlRules/Figures/")
+dat3 <- read.csv("PercentDiffs.csv",header=T)
 palette <- brewer.pal(6,"Spectral")
 hcr.colors <- palette[c(6,5,4,3,1,2)]
 
@@ -33,23 +36,29 @@ unique(dat3$variable)
 dat3$name <- dat3$variable
 dat3$name <- factor(dat3$name, levels=c("SDcatch","nyrs0catch","n.5yrclose","CollapseLength","Prob.Collapse","Collapse.Severity","LTmeancatch", "meanbiomass", "BonanzaLength"))
 
-dat3 <- mutate(dat3,name = recode (name, 'LTmeancatch' = "Mean catch",
+# Multiply all the "negative" performance measures by -1 so they're all scaled to make positive = good
+negativos <- c("SDcatch","nyrs0catch","n.5yrclose","CollapseLength","Prob.Collapse","Collapse.Severity")
+dat3.new <- dat3
+dat3.new$percentdiff[which(dat3$variable %in% negativos)] <- -1 * dat3.new$percentdiff[which(dat3$variable %in% negativos)]
+
+# Change names to nice labels so they make sense
+dat3.new <- mutate(dat3.new,name = recode(name, 'LTmeancatch' = "Mean catch",
                                        "meanbiomass" = "Mean biomass",
                                        "BonanzaLength" = "Bonanza Length",
-                                       'SDcatch' = "Catch variation",
-                                       'nyrs0catch' = "Years with 0 catch",
-                                       'n.5yrclose' = "P(5 yr closure|closure)",
-                  "CollapseLength" = "Collapse length",
-                  "Prob.Collapse" = "P(collapse)",
-                  "Collapse.Severity" = "Collapse severity"))
+                                       'SDcatch' = "Minimize catch variation",
+                                       'nyrs0catch' = "Minimize years with 0 catch",
+                                       'n.5yrclose' = "Minimize P(5 yr closure|closure)",
+                  "CollapseLength" = "Minimize collapse length",
+                  "Prob.Collapse" = "Minimize P(collapse)",
+                  "Collapse.Severity" = "Minimize collapse severity"))
 
 setwd("/Users/mcsiple/Dropbox/Chapter4-HarvestControlRules/Figures")
-pdf(file = "PercentDiffsErrors.pdf",width = 10,height=9,useDingbats = FALSE)
-ggplot(dat3, aes(x=name,y=percentdiff)) +
+pdf(file = "PercentDiffsErrors.pdf",width = 10,height = 9,useDingbats = FALSE)
+ggplot(dat3.new, aes(x=name,y=percentdiff)) +
   geom_bar(colour='black',aes(fill=HCR),stat = "identity") + 
   scale_fill_manual(values = hcr.colors[c(1,2,3,6,4,5)]) +
   geom_hline(yintercept=0)+facet_grid(HCR~Type) +
-  geom_vline(xintercept=6.5)+
+  geom_vline(xintercept = 6.5) +
   theme_classic(base_size = 14) + 
   theme(strip.background = element_blank(),strip.text.y = element_blank()) +
   #theme(axis.text.x = element_text(angle = 90, hjust = 1,vjust=0.2)) +
