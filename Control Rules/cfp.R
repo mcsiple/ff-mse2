386ee4b3cff941ea1e0e45bdf4bf4c8a76bb2098
Megsie Siple <mcsiple@gmail.com>
2018-01-22 21:37:21 -0800

    fixed notes and added places 2 check

--- Control Rules/cfp.R
+++ Control Rules/cfp.R
@@ -21,17 +21,16 @@ calc.F.cfp <- function(prevCatch, Bobs, Btru, Btarget, Blim, Fmax, lh = NA, sel.
   if (sum(Bobs) > Blim & sum(Bobs) <= Btarget) {f <- slope * (sum(Bobs)-Blim) / adj.constant} 
                                       # adj.constant scales so the CR is linear btwn Blim and Btarget
   if (sum(Bobs) > Btarget) {f = Fmax }
-  # possible.catch <- f*Bt # old possible.catch
+  
   # Result of above is F from the basic hockey stick rule. Now find out if catch would change >15%
   possible.catch <- sum(Bobs *(1-exp(-(f*sel.at.age[,1]+lh$M)))*f*sel.at.age[,1] / (f*sel.at.age[,1] + lh$M) ) # Baranov catch eqn
   newcatch <- possible.catch
   if(possible.catch != 0 && possible.catch < 0.85*prevCatch) {newcatch <- 0.85*prevCatch}
   if(possible.catch != 0 && possible.catch > 1.15*prevCatch) {newcatch <- 1.15*prevCatch}  # I checked whether 30% change (instead of 15%) works similarly; it didn't make a huge difference in relative performance.
   if(newcatch > sum(Btru[-1])){newcatch = sum(Btru[-1]) * 0.5}
-  # This used to allow catches not to increase either-this was causing issues so I changed it.
+  # *** TODO: Need to check the above. If catches are higher than biomass, halve them? Need to test again as newcatch = sum(Btru[-1])
   # Get the f from the control rule
   f.new <- calc.true.f(tac.fn = newcatch,M.fn = lh$M,sel.fn = sel.at.age[,1],Btrue = Btru, w.at.age = sizes$weight.at.age[,1])
-  #f <- newcatch / Bt
   return(f.new)
 }
 
