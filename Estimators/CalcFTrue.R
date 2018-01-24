01203140fe4f30723458bdc810acac1dd2d563df
Margaret Siple <mcsiple@gmail.com>
2018-01-23 16:42:14 -0800

    adding doc'tion

--- Estimators/CalcFTrue.R
+++ Estimators/CalcFTrue.R
@@ -1,4 +1,4 @@
-# This function estimates F_true, the true fishing rate based on the TAC. This is different than F from the HCR, because the TAC is determined with the assumption that the observed biomass is correct, i.e.,
+# This function estimates F_true, the true fishing rate based on the TAC. This is different than F from the HCR, because the TAC is determined with the assumption that the observed biomass is correct
 
 # EXAMPLE - SEVEN AGES TOTAL
       # tac=20000
@@ -7,9 +7,13 @@
       # obs.at.age.oneplus <- c(39869.064, 22180.144, 13353.527,  7767.168,  6295.520,  3154.487) # this is a vector of 6, has to be bc of errors
       # w.at.age <- c(0.0001281, 0.0002317, 0.0003285, 0.0003711, 0.0005371, 0.0004481)
 
-calc.true.f <- function(tac.fn = tac, M.fn = M, sel.fn = sel, Btrue = NA, w.at.age =w.at.age){
+calc.true.f <- function(tac.fn = tac, M.fn = M, sel.fn = sel, Btrue = NA, w.at.age = w.at.age){
         fn.to.minimize <- function(param,tac = tac.fn, M = M.fn, sel = sel.fn, Bobs_a.fn = Bobs_a,
                                    w.at.age.fn = w.at.age) {
+            #' @param tac.fn - the TAC (calculate from B_obs and F.intended)
+            #' @param Btrue - vector of true biomass at age in year y
+            #' 
+ 
           F.true <- param
           death_rate <- M + sel * F.true
           catch_age <- (Btrue * sel * F.true * (1-exp(-death_rate))) / death_rate
