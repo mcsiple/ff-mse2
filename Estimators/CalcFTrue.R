df9c32a989d4ffe3936bd8333ec7f999de961bde
Margaret Siple <mcsiple@gmail.com>
2018-01-24 14:03:00 -0800

    documentation, fixed catch equation (no change to analysis)

--- Estimators/CalcFTrue.R
+++ Estimators/CalcFTrue.R
@@ -1,25 +1,27 @@
-# This function estimates F_true, the true fishing rate based on the TAC. This is different than F from the HCR, because the TAC is determined with the assumption that the observed biomass is correct
+# This function estimates F_true, the true fishing rate based on the TAC. This is because the true F is the connection between the estimation and operating models.
+# This "true F" is different than F from the HCR (the "intended F"), because the TAC is determined by "managers" in the model with the assumption that observed biomass is correct.
 
-# EXAMPLE - SEVEN AGES TOTAL
-      # tac=20000
+# EXAMPLE W/ SEVEN AGES TOTAL
+      # tac = 20000
       # M = 0.5
       # sel <-  c(0.004, 0.143, 0.994, 0.840, 0.191, 0.024, 0.000)
       # obs.at.age.oneplus <- c(39869.064, 22180.144, 13353.527,  7767.168,  6295.520,  3154.487) # this is a vector of 6, has to be bc of errors
       # w.at.age <- c(0.0001281, 0.0002317, 0.0003285, 0.0003711, 0.0005371, 0.0004481)
 
-calc.true.f <- function(tac.fn = tac, M.fn = M, sel.fn = sel, Btrue = NA, w.at.age = w.at.age){
-        fn.to.minimize <- function(param,tac = tac.fn, M = M.fn, sel = sel.fn, Bobs_a.fn = Bobs_a,
-                                   w.at.age.fn = w.at.age) {
-            #' @param tac.fn - the TAC (calculate from B_obs and F.intended)
-            #' @param Btrue - vector of true biomass at age in year y
-            #' 
- 
-          F.true <- param
+calc.true.f <- function(tac.fn = tac, M.fn = M, sel.fn = sel, Btrue.fn = NA, w.at.age = w.at.age){
+        #' @description given the TAC, and the "true" biomass, what is the true F that will result in catches = TAC?
+        #' @param tac.fn - the TAC (calculate from B_obs and F.intended)
+        #' @param M.fn - natural mortality; drawn from OM 
+        #' @param sel.fn - vector of selectivity at age
+        #' @param Btrue.fn - vector of true biomass at age in yr y
+        #' @param w.at.age - weight at age; drawn from OM
+        fn.to.minimize <- function(param,tac = tac.fn, M = M.fn, sel = sel.fn,
+                                   w.at.age.fn = w.at.age, Btrue = Btrue.fn) {
+          F.true <- param       # solving for F
           death_rate <- M + sel * F.true
-          catch_age <- (Btrue * sel * F.true * (1-exp(-death_rate))) / death_rate
-          #catch_age <- Bobs_a * (1-exp(M+sel*F.true)) / (M + F.true) # allowable catch at age
+          catch_age <- (Btrue * sel * F.true * (1-exp(-death_rate))) / death_rate # this is the biomass at age caught by the fishery
           pred.tac <- sum(catch_age)
-          diff <- (sum(tac) - pred.tac)^2
+          diff <- (sum(tac) - pred.tac)^2   # for optim purposes
           return(diff)
         }
   est <- optim(par=0.5, fn=fn.to.minimize, method="BFGS")
