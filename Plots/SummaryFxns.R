ea006f4fb7fe1e1caaddca3d7e9805ec410f3a62
Margaret Siple <mcsiple@gmail.com>
2018-02-06 13:21:04 -0800

    add ifelse() to index runs with over- or under-estimates of reference points

--- Plots/SummaryFxns.R
+++ Plots/SummaryFxns.R
@@ -143,10 +143,12 @@ n.multiyr.closures <- function(x, threshold = NA) { #where x is a matrix, rows a
 
 
 # Get summary metrics so you can make Zeh plots (or whatever you want)
-summ.tab <- function(result.list, individual.sim = FALSE){ #result.list is one of the results (=1 harvest rule, 1000 time series of biomass, catch, fishing, rec, depl)
+summ.tab <- function(result.list, individual.sim = FALSE,calc.ind = calc.ind, oe.ind = NA){ #result.list is one of the results (=1 harvest rule, 1000 time series of biomass, catch, fishing, rec, depl)
   for(i in 1:length(result.list)){
-    result.list[[i]] <- result.list[[i]][,calc.ind]
-  } # Trim results to the years we're using
+    result.list[[i]] <- ifelse(!is.na(oe.ind),result.list[[i]][oe.ind,calc.ind],
+                                                result.list[[i]][,calc.ind])
+    }  # Trim results to the years we're using
+
   LTmeans.list <- lapply(result.list,FUN = rowMeans,na.rm=TRUE) 
   # median and quantiles of LTM of all PMs
   ltm <- lapply(LTmeans.list,FUN = quantile, probs = c(0.05,0.5,0.95),na.rm=TRUE) 
