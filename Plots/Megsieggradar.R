faf7965639fc5fe42cd862d5568404b6d5504521
Margaret Siple <mcsiple@gmail.com>
2018-03-01 15:49:39 -0800

    add option for manually ordering legend labels

--- Plots/Megsieggradar.R
+++ Plots/Megsieggradar.R
@@ -39,13 +39,15 @@ ggradar <- function(plot.data,
                     plot.legend=if (nrow(plot.data)>1) TRUE else FALSE,
                     legend.title="",
                     legend.text.size=grid.label.size,
-                    palette.vec = c("#D53E4F", "#FC8D59", "#FEE08B", "#E6F598", "#99D594", "#3288BD")) {
+                    palette.vec = c("#D53E4F", "#FC8D59", "#FEE08B", "#E6F598", "#99D594", "#3288BD"),
+                    manual.levels = NA) {
   
   library(ggplot2)
   
   plot.data <- as.data.frame(plot.data)
   
   plot.data[,1] <- as.factor(as.character(plot.data[,1]))
+  if(all(!is.na(manual.levels))){plot.data[,1] <- factor(plot.data[,1],levels = manual.levels)}
   names(plot.data)[1] <- "group"
   
   var.names <- colnames(plot.data)[-1]  #'Short version of variable names 
