33b9ad36792183ec05e70540353c510656b3ee40
Megsie Siple <mcsiple@gmail.com>
2019-03-06 10:40:08 -0800

    this shows the expected collapses under a no-fishing scenario for real forage fish pops

--- Plots/PlotCollapses.R
+++ Plots/PlotCollapses.R
@@ -1,5 +1,8 @@
 # This code is to figure out whether the rec devs we made up generate similar biomass dynamics to real populations, without fishing. I.e., do they seem to generate the same number and frequency of collapses without fishing, as real populations would experience w/o fishing? I fed RAM recruitment estimates straight into the age-structured model to see.
 
+
+# FIGURE S7 ---------------------------------------------------------------
+
 # Set directories
                   basedir <- "/Users/mcsiple/Dropbox/Chapter4-HarvestControlRules/Code/ff-mse2"
                   resultsdir <- "/Users/mcsiple/Dropbox/Chapter4-HarvestControlRules/Results"
@@ -17,9 +20,10 @@
                   # Load rev devs generating fxn, MSE main model, estimator fxns
                   toplot=FALSE      # Don't plot examples of rec trajectories
                   source(file.path(basedir,"Recruitment/GenerateDevs.R")) 
-                  source(file.path("/Users/mcsiple/Dropbox/Chapter4-HarvestControlRules/Code/HCR_Trajectory.R"))
+                  source(file.path(basedir,"Run/HCR_Trajectory_NEW.R"))
                   source(file.path(basedir,"Estimators/Estimators.R"))
                   source(file.path(basedir,"Run/generate_M.R"))
+                  source(file.path(basedir,"Estimators/CalcFTrue.R"))
                   
                   # Load harvest rules
                   source(file.path(basedir,"Control Rules/smith_oceana.R"))
@@ -27,14 +31,14 @@
                   source(file.path(basedir,"Control Rules/hockey-stick.R"))
                   source(file.path(basedir,"Control Rules/trend-based-rule.R"))
                   
-
+                  
 ffdat <- read.csv("/Users/mcsiple/Dropbox/Chapter4-HarvestControlRules/Datasets/allforagedata.csv")
 
 library(ggplot2)
 library(plyr)
 library(dplyr)
 
-ggplot(ffdat,aes(x=Years,y=R)) + geom_line()+geom_point() +facet_wrap(~ASSESSID,scales="free_y")
+ggplot(ffdat,aes(x=Years,y=R)) + geom_line()+facet_wrap(~ASSESSID,scales="free_y")
 tslength <- ffdat %>% group_by(ASSESSID) %>% subset(!is.na(R)) %>% summarize(tslength = max(Years)-min(Years),minyr=min(Years),maxyr=max(Years)) %>% as.data.frame()
 
 max(tslength$tslength)
@@ -74,6 +78,13 @@ for(i in 1:nstocks){
   steepness = 0.6
   obs.type <- "AC"
   HCR <- "constF"
+  # Create errors for AC scenario
+  set.seed(123)
+  curly.phi.mat <- matrix(NA, nrow = nsims,ncol = years.test)
+  for(sim in 1:nsims){
+    curly.phi.mat[sim,] <- rnorm(years.test,0,sig.s)
+  }
+  
   # Get LH characteristics for that ff type
             # Sardines
             if(type == "Sardine"){
@@ -91,13 +102,17 @@ for(i in 1:nstocks){
               source(file.path(basedir,"Ctl/Menhaden_FisheryControl.R"))
             }
             }}
+  
+  ac.params = list(rho=ifelse(recruit.rho==0.9,0.89,0.74),
+                   sig.s=ifelse(recruit.rho==0.9,0.34,0.37))    #NEW- give sardine long-lived params
+  
   equilib = getEquilibriumConditions(lh = lh.test,fish = seq(0,5,by=.1),years = 150,steepness=steepness)
   #rec.dev.test <- generate.devs(N = years.test,rho = recruit.rho,sd.devs = recruit.sd)
-  test.constF <- calc.trajectory(lh = lh.test,obs.cv = 1.2, init = init.test, rec.dev = NA,rec.ram = rec.ram.test, F0 = F0.test, cr = cr.test, years = years.to.plot,hcr.type = "constF", const.f.rate = 0, steepness = steepness,obs.type = obs.type,equilib=equilib,R0.traj = R0.sens, tim.params = tim.params,time.var.m = NA)
-  nofish <- melt(test.constF[-c(1,4,9,10)])
-  nofish$year <- rep(actual.years,times=length(unique(nofish$L1)))
+  test.constF <- calc.trajectory(lh = lh.test,obs.cv = 1.2, init = init.test, rec.dev = NA,rec.ram = rec.ram.test, F0 = F0.test, cr = cr.test, years = years.to.plot,hcr.type = "constF", const.f.rate = 0, steepness = steepness,obs.type = obs.type,equilib=equilib,R0.traj = R0.sens, tim.params = tim.params,time.var.m = NA,sig.s = 0.3,curly.phi.vec = curly.phi.mat[sim,],ac.params = ac.params)$biomass.oneplus.true
+  nofish <- melt(test.constF) # unfished biomass
   nofish$stock <- stocks[i]
-  mf <- subset(nofish,L1=="biomass.oneplus.true")
+  nofish$year <- actual.years
+  mf <- nofish
   mf$thresh <- (mean(mf$value))*0.2           #"Collapse" threshold - always based on unfished biomass
   mf$bm <- mean(mf$value)                     # mean biomass, in case you want to plot it
   mf$type <- type
@@ -122,7 +137,7 @@ baseplot <- ggplot(bigframe,aes(x=year,y=value,colour=type)) + geom_line() + geo
   xlab("Year") + theme_classic(base_size = 12) 
 
 # Plot thresholds and stuff - doesn't make a difference what the obs model is.
-pdf("B_oneplus_true_RAM_AC.pdf",width=14,height =9)
+pdf("FIGURE_S7_B_oneplus_true_RAM_AC.pdf",width=14,height =9)
 baseplot + 
   geom_label(x = 2000, aes(y=ycoord, label = ncolls),size=3,colour="black",data=labels) +
   theme(strip.background = element_rect(colour="white"))
